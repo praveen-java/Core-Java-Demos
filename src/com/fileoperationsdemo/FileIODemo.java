93f0f695836f138ba0def079f6dd3ea4bc89bc00
praveen-java <37608007+praveen-java@users.noreply.github.com>
2018-03-21 05:27:01 +0530

    File status Check.

--- src/com/fileoperationsdemo/FileIODemo.java
+++ src/com/fileoperationsdemo/FileIODemo.java
@@ -1,9 +1,30 @@
 package com.fileoperationsdemo;
 
+import java.io.File;
+
 public class FileIODemo {
 
 	public static void main(String[] args) {
+		System.out.println("This is to demo file operations.");
 		
+		try {
+			File file = new File("MyNewFile.txt");
+			System.out.println(file.canRead());
+			System.out.println(file.canWrite());
+			System.out.println(file.createNewFile());
+			System.out.println(file.canRead());
+			System.out.println(file.canWrite());
+			System.out.println(file.lastModified());
+			System.out.println(file.length());
+			System.out.println(file.exists());
+			System.out.println(file.isFile());
+			System.out.println(file.isDirectory());
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.getCanonicalPath());
+			System.out.println(file.delete());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
