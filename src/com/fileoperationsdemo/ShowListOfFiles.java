f159ae266d69fd79ca3d357b7a363ebe282e51c2
praveen-java <37608007+praveen-java@users.noreply.github.com>
2018-03-21 11:37:19 +0530

    File to show Directory Contents

---
+++ src/com/fileoperationsdemo/ShowListOfFiles.java
@@ -0,0 +1,16 @@
+package com.fileoperationsdemo;
+
+import java.io.File;
+
+public class ShowListOfFiles {
+
+	public static void main(String[] args) {
+		System.out.println("This is to list all files.");
+		File file = new File("D:\\books");
+		String[] stringList = file.list();
+		for(String s : stringList){
+			System.out.println(s);
+		}
+	}
+
+}
