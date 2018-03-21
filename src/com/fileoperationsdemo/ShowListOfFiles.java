93fb6d59d7b9e6be229a4df10b4f0b923b318dc8
praveen-java <37608007+praveen-java@users.noreply.github.com>
2018-03-21 11:47:12 +0530

    To list all files.

--- src/com/fileoperationsdemo/ShowListOfFiles.java
+++ src/com/fileoperationsdemo/ShowListOfFiles.java
@@ -7,10 +7,15 @@ public class ShowListOfFiles {
 	public static void main(String[] args) {
 		System.out.println("This is to list all files.");
 		File file = new File("D:\\books");
-		String[] stringList = file.list();
+		/*String[] stringList = file.list();
 		for(String s : stringList){
 			System.out.println(s);
+		}*/
+		File[]	listFiles = file.listFiles();
+		for(File f:listFiles){
+			System.out.println(f.getName());
 		}
+		
 	}
 
 }
